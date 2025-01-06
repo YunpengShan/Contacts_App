44c8c46637fd944f88aac779d19170a441dc9773
Yunpeng Shan <shanyunp@sheridancollege.ca>
2025-01-05 21:24:21 -0500

    added controllers

---
+++ API/Contactly/Controllers/ContactsController.cs
@@ -0,0 +1,44 @@
+﻿using Contactly.Data;
+using Contactly.Models;
+using Contactly.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contactly.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly ContactlyDbContext dbContext;
+
+        public ContactsController(ContactlyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllContacts()
+        {
+            var contacts = dbContext.Contacts.ToList();
+            return Ok(contacts);
+        }
+
+        [HttpPost]
+        public IActionResult AddContact(AddContactRequestDTO requst)
+        {
+            var domainModelContact = new Contact
+            {
+                Id = Guid.NewGuid(),
+                Name = requst.Name,
+                Email = requst.Email,
+                Phone = requst.Phone,
+                Favorite = requst.Favorite,
+            };
+
+            dbContext.Contacts.Add(domainModelContact);
+            dbContext.SaveChanges();
+            return Ok(domainModelContact);
+        }
+    }
+}
